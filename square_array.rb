670d51e2f933584fbae2ebc625039a0f539e468e
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-17 23:27:38 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -6,6 +6,10 @@ def square_array(array)
     new_array[index] = num ** 2
     index += 1
   end
-  
+
   return new_array
+end
+
+def square_array_collect(array)
+  array.collect {|num| num ** 2 }
 end
